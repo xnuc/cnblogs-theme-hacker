b5836e0c4f28eef8ac6d6c8fe9db09e413c7fe92
mosszzom <mosszzom@outlook.com>
2021-09-28 08:10:52 +0800

    feat: 评论功能

--- plugins/comment.js
+++ plugins/comment.js
@@ -1,8 +1,11 @@
 export function GetCommentFromRsp(rsp) {
     const comments = []
-    const matches = rsp.matchAll(/\s<div class="feedbackCon"><div id="comment_body_[0-9]*?" data-format-type="Markdown" class="blog_comment_body cnblogs-markdown">([\s\S]*?)<\/div>\s<div class="comment_vote">/g)
-    Array.from(matches).forEach(e => {
-        comments.push({content: e[1].trim()})
-    })
+    let matches = rsp.matchAll(/\s<div class="feedbackCon">\s*?<div id="comment_body_([0-9]*?)" data-format-type="Markdown" class="blog_comment_body cnblogs-markdown">([\s\S]*?)<\/div>\s*?<div class="comment_vote">/g)
+    const contents = Array.from(matches)
+    matches = rsp.matchAll(/<a href="#([0-9]*?)" class="layer">\S([0-9]*?)\S<\/a>/g)
+    const layers = Array.from(matches)
+    for (let idx = 0; idx < Math.min(contents.length, layers.length); idx++) {
+        comments.push({commentID: layers[idx][1], layer: parseInt(layers[idx][2]), content: contents[idx][2].trim()})
+    }
     return comments
 }
--- plugins/comment_test.js
+++ plugins/comment_test.js
@@ -1,15 +1,17 @@
 import {GetCommentFromRsp} from "./comment";
 
-const rsp = `<div class="feedbackCon">
+const rsp = `
+<a href="#4943655" class="layer">#3楼</a>
+<div class="feedbackCon">
 <div id="comment_body_4943655" data-format-type="Markdown" class="blog_comment_body cnblogs-markdown">
 <p>看不太懂，不过支持一下吧</p>
-</div>`
+</div>
+<div class="comment_vote">`
 
 // @case 从 rsp 匹配到用户的评论
 export async function Test_GetCommentFromRsp() {
     const comments = GetCommentFromRsp(rsp)
-    const expect = [
-        {content: `<p>看不太懂，不过支持一下吧</p>`},
-    ]
+    const expect =
+        [{layer: 3, content: `<p>看不太懂，不过支持一下吧</p>`}]
     console.assert(JSON.stringify(expect) === JSON.stringify(comments), {expect, comments})
 }
